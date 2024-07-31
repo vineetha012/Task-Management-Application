5707a2937caa7011f1ebc5c4438c600f70251477
vineetha012 <yenugulavineetha2001@gmail.com>
2024-07-31 19:47:35 +0530

    merged

--- task-managemrnt-server/src/config/index.js
+++ task-managemrnt-server/src/config/index.js
@@ -2,8 +2,8 @@ const dotenv=require('dotenv')
 dotenv.config({})
 const envInfo = process.env
 const config={
-    MONGO_URI: envInfo.MONGO_URI,
-    PORT: envInfo.PORT,
+    MONGO_URI: envInfo.MONGO_URI || 'mongodb + srv://vineetha:vineetha10x@vineetha-cluster.inwcmoa.mongodb.net/task-management?retryWrites=true&w=majority',
+    PORT: envInfo.PORT||8080,
     SALT:10,
     JWT_SECRET:'taskmngmnt012'
 }
